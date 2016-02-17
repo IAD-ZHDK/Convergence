c404302898285cc16ca251178da0704503e613c9
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-02-17 19:01:14 +0100

    added not found and error files

--- convergence.go
+++ convergence.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"fmt"
 )
 
 type Convergence struct {
@@ -23,18 +24,20 @@ func (c *Convergence) Run() error {
 	router.GET("/page/:key", c.space)
 	router.GET("/page/:key/:title", c.page)
 
+	router.NoRoute(c.notFound)
+
 	return router.Run()
 }
 
 func (c *Convergence) root(ctx *gin.Context) {
 	spaces, err := c.Confluence.GetSpaces()
 	if err != nil {
-		ctx.AbortWithError(503, err)
+		c.error(ctx, err)
 		return
 	}
 
 	ctx.HTML(http.StatusOK, "index.html", gin.H{
-		"Title": "Convergence",
+		"Title": "IAD Wiki",
 		"Spaces": spaces,
 	})
 }
@@ -44,13 +47,13 @@ func (c *Convergence) space(ctx *gin.Context) {
 
 	space, err := c.Confluence.GetSpace(key)
 	if err != nil {
-		ctx.AbortWithError(503, err)
+		c.error(ctx, err)
 		return
 	}
 
 	page, err := c.Confluence.GetPageById(key, space.HomepageID)
 	if err != nil {
-		ctx.AbortWithError(503, err)
+		c.error(ctx, err)
 		return
 	}
 
@@ -63,9 +66,23 @@ func (c *Convergence) page(ctx *gin.Context) {
 
 	page, err := c.Confluence.GetPageByTitle(key, title)
 	if err != nil {
-		ctx.AbortWithError(503, err)
+		c.error(ctx, err)
 		return
 	}
 
 	ctx.HTML(http.StatusOK, "page.html", page)
+}
+
+func (c *Convergence) error(ctx *gin.Context, err error) {
+	fmt.Printf("Error: %s\n", err.Error())
+
+	ctx.HTML(http.StatusInternalServerError, "503.html", gin.H{
+		"Title": "Internal Server Error",
+	})
+}
+
+func (c *Convergence) notFound(ctx *gin.Context) {
+	ctx.HTML(http.StatusOK, "404.html", gin.H{
+		"Title": "Not Found",
+	})
 }
