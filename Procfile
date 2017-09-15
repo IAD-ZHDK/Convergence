ba0e71b40d1f02dbe7c2b75bc646d306ccaedf34
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-09-15 12:02:18 +0200

    try proper buildpack compilation

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: ./bin/convergence
+web: ./convergence
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# gp run ./build.sh
-gox -osarch="linux/amd64" -output="./bin/convergence" github.com/IAD-ZHDK/Convergence
